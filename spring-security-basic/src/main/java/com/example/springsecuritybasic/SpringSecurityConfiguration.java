a25a4ee2aa3536c6ac2a1ddb0b1d85f037c9e6db
yusuf abdullah <yusuf@2u.chat>
2022-03-01 10:38:23 +0300

    feat: adds admin mode and user mode.

---
+++ spring-security-basic/src/main/java/com/example/springsecuritybasic/SpringSecurityConfiguration.java
@@ -0,0 +1,42 @@
+package com.example.springsecuritybasic;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfiguration  extends WebSecurityConfigurerAdapter{
+
+    @Autowired
+    CustomUserDetailsService userDetailsService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception
+    {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        // We don't need CSRF for this example
+        httpSecurity.csrf().disable()
+                .authorizeRequests().antMatchers("/helloadmin")
+                .hasRole("ADMIN")
+                .antMatchers("/hellouser")
+                .hasAnyRole("ADMIN","USER")
+                .and().httpBasic();
+    }
+
+}
