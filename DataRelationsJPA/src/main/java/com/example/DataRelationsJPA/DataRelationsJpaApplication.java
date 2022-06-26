03dcd5340476e10b759fb585adbf41c1b5e8997c
yusuf abdullah arslanalp <ayusufa42@gmail.com>
2022-06-26 12:24:17 +0300

    initial commit for DataRelationsJPA project

---
+++ DataRelationsJPA/src/main/java/com/example/DataRelationsJPA/DataRelationsJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.DataRelationsJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataRelationsJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataRelationsJpaApplication.class, args);
+	}
+
+}
---
+++ DataRelationsJPA/src/test/java/com/example/DataRelationsJPA/DataRelationsJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DataRelationsJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataRelationsJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
