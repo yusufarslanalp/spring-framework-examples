1b434de67b277ce3fb30dbbd34214494f520cd43
ayusufa42@gmail.com <ayusufa42@gmail.com>
2023-02-03 16:41:27 +0300

    new project: add Transaction project

---
+++ Transaction/src/main/java/main_package/Main.java
@@ -0,0 +1,13 @@
+package main_package;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+}
