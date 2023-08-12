d58a2c951925867bb277caae770913802c61e5a6
yusufarslanalp <ayusufa42@gmail.com>
2023-08-12 17:29:01 +0300

    add httpClient project

---
+++ httpClient/src/main/java/com/example/httpClient/HttpClientApplication.java
@@ -0,0 +1,24 @@
+package com.example.httpClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@RestController
+public class HttpClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpClientApplication.class, args);
+	}
+
+	@GetMapping("/restTemplate")
+	public String greet(  ){
+		RestTemplate restTemplate = new RestTemplate();
+		String s = restTemplate.getForObject("http://localhost:8081/greet", String.class);
+		return s + " from http clint project";
+	}
+}
---
+++ httpClient/src/test/java/com/example/httpClient/HttpClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.httpClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
